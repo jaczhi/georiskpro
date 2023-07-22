2d9987c2d86d60498665d5072b7fbb3809d58403
Jacob Zhi <jacobazhi@gmail.com>
2023-07-22 11:33:46 -0700

    create json and month dropdown

--- widgets/routeproc/src/runtime/widget.tsx
+++ widgets/routeproc/src/runtime/widget.tsx
@@ -1,10 +1,12 @@
 import { React, AllWidgetProps } from 'jimu-core'
 import { JimuMapViewComponent, JimuMapView } from 'jimu-arcgis'
 import FeatureLayer from 'esri/layers/FeatureLayer'
+
 const { useState } = React
 
 const Widget = (props: AllWidgetProps<any>) => {
   const [jimuMapView, setJimuMapView] = useState<JimuMapView>()
+  const [month, setMonth] = useState("");
 
   const activeViewChangeHandler = (jmv: JimuMapView) => {
     if (jmv) {
@@ -12,28 +14,67 @@ const Widget = (props: AllWidgetProps<any>) => {
     }
   }
 
+  const registerMonth= (e) => {
+    e.preventDefault();
+    if(!e.target.value) {
+      // do nothing
+    }
+    else if (e.target.value === "January") {
+      // create a new FeatureLayer (dummy layer for now)
+      const layer = new FeatureLayer({
+        url: 'https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0'
+      })
+      // Add the layer to the map (accessed through the Experience Builder JimuMapView data source)
+      jimuMapView.view.map.add(layer)
+      console.log("should have been added by now...")
+    }
+    setMonth(e.target.value);
+  }
+
   const formSubmit = (evt) => {
     evt.preventDefault()
 
-    // create a new FeatureLayer
-    const layer = new FeatureLayer({
-      url: 'https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0'
-    })
 
-    // Add the layer to the map (accessed through the Experience Builder JimuMapView data source)
-    //jimuMapView.view.map.add(layer)
+
+
+    var result = {
+      directionline:[],
+      directionpoint:[],
+    };
+    
+    
     const layerViews = jimuMapView.view.allLayerViews
     for(const layerView of layerViews) {
       const layer = layerView.layer
       console.log(layer.title + ": type " + layer.type);
       if (layer.type === "route") {
         console.log("Route found!")
-        // TODO put this in a JSON and send an HTTP request. 
+        // TODO put this in a JSON and send an HTTP request.
+        
         for (const directionLine of layer.directionLines) {
-          console.log("leg, distance: " + directionLine.distance)
-          console.log("sptaial reference wkid: " + directionLine.geometry.spatialReference.wkid)
-          console.log(directionLine.geometry.paths)
+          // console.log("leg, distance: " + directionLine.distance)
+          // console.log("sptaial reference wkid: " + directionLine.geometry.spatialReference.wkid)
+          // console.log(directionLine.geometry.paths)
+          result.directionline.push({
+            distance: directionLine.distance,
+            geometry: directionLine.geometry.toJSON(),
+          })
         }
+
+  
+
+        for (const directionPoint of layer.directionPoints) {
+          console.log("direction points"+ directionPoint.displayText)
+          console.log("sptaial reference wkid: " + directionPoint.geometry.spatialReference.wkid)
+          console.log(directionPoint.geometry.paths)
+          result.directionpoint.push({
+            directionPointType: directionPoint.directionPointType,
+            displayText: directionPoint.displayText,
+            geometry: directionPoint.geometry.toJSON()
+          })
+        }
+        // TODO: send here. 
+        console.log(JSON.stringify(result))
       }
     }
   }
@@ -52,7 +93,13 @@ const Widget = (props: AllWidgetProps<any>) => {
 
       <form onSubmit={formSubmit}>
         <div>
-          <button>Add Layer</button>
+          <p>Select Month</p>
+        <select name="month" value={month} onChange={event => registerMonth(event)}>
+            <option id="0" ></option>
+            <option id="1" >January</option>
+            <option id="2" >Febuary</option>
+        </select>
+          <button>Process Route</button>
         </div>
       </form>
     </div>
@@ -60,4 +107,3 @@ const Widget = (props: AllWidgetProps<any>) => {
 }
 
 export default Widget
-
