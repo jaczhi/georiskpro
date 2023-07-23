d9ed166b89e3c4bc406059bf4631f1211ce31ff3
Ritik Kapoor <rkapoor@esri.com>
2023-07-23 10:09:39 -0700

    hurricane part

--- python_server.py
+++ python_server.py
@@ -82,30 +82,34 @@ class JSONHandler(http.server.BaseHTTPRequestHandler):
 
             #earthquake = shapefile.Reader("C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\earthquakesRisk\\earthquakes\\earthquakeRisk.shp")
             
-            folder_path = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output"
+           # folder_path = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output"
            # os.mkdir(folder_path)
 
-            items = os.listdir(folder_path)
+            #items = os.listdir(folder_path)
         
             # Check if the folder is empty
-            try:
-                if len(items) != 0:
+            #try:
+             #   if len(items) != 0:
                     # Loop through each item and delete files or remove directories
-                    for item in items:
-                        item_path = os.path.join(folder_path, item)
-                        if os.path.isfile(item_path):
+              #      for item in items:
+               #         item_path = os.path.join(folder_path, item)
+                #        if os.path.isfile(item_path):
                             # Delete the file
-                            os.remove(item_path)
-            except:
-                pass
+                 #           os.remove(item_path)
+            #except:
+            #    pass
+
+            ## Eathquake  ###########################################################################
             
-            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output"
+            infeature1 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp"
+            infeature2 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\Earthquake_Data\\Earthquake_Data\\EarthquakeRisk.shp"
+            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output_earthquake"
             timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
             folder_path = os.path.join(base_folder, timestamp)
             os.mkdir(folder_path)
             ttemp = (os.path.join(folder_path,timestamp))
             # Process: Find all stream crossings (points)
-            inFeatures = ["C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp", r"C:\Users\rit13425\Desktop\hackathon\georiskpro\Earthquake_Data\Earthquake_Data\EarthquakeRisk.shp"]
+            inFeatures = [infeature1, infeature2]
             intersectOutput = ttemp #os.path.join(folder_path,timestamp) #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output"
             arcpy.analysis.Intersect(inFeatures, intersectOutput, "", "", "point")
 
@@ -136,9 +140,198 @@ class JSONHandler(http.server.BaseHTTPRequestHandler):
                     attribute1 = row[1]  # The value of "field1" attribute for the feature
                     attribute2 = row[2]  # The value of "field2" attribute for the feature
                     print(attribute2)
-                    data_points.append((attribute2,attribute1))
+                    data_points.append(("Earthquake",attribute2,attribute1))
+
+            ## Eathquake  ###########################################################################
 
 
+            ## Hurricane  MAY ###########################################################################
+
+            infeature1 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp"
+            infeature2 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_MAY.shp"
+            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output_hurricane"
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            folder_path = os.path.join(base_folder, timestamp)
+            os.mkdir(folder_path)
+            ttemp = (os.path.join(folder_path,timestamp))
+            # Process: Find all stream crossings (points)
+            inFeatures = [infeature1, infeature2]
+            intersectOutput = ttemp #os.path.join(folder_path,timestamp) #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output"
+            arcpy.analysis.Intersect(inFeatures, intersectOutput, "", "", "point")
+
+            #print(os.path.join(ttemp + timestamp + ".sh"))
+            print(os.path.join(ttemp + ".shp"))
+            arcpy.management.AddJoin(
+                in_layer_or_view= os.path.join(ttemp + ".shp") , #s.path.join(base_folder, timestamp + ".sh"), #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp",
+                in_field="Id_1",
+                join_table= r"C:\Users\rit13425\Desktop\hackathon\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_MAY.shp",
+                join_field="Id",
+                index_join_fields="NO_INDEX_JOIN_FIELDS",
+                rebuild_index="NO_REBUILD_INDEX",
+                join_type="KEEP_COMMON"
+            )
+
+            # Path to the shapefile you want to read
+            shapefile_path = os.path.join(ttemp + ".shp")#r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp"
+
+           # data_points = []
+
+            # Open a SearchCursor to read the shapefile
+            fields = ["SHAPE@", "gridcode","Shape","Explain"]  # Replace field1, field2, ... with the field names you want to access
+            with arcpy.da.SearchCursor(shapefile_path, fields) as cursor:
+                for row in cursor:
+                    
+                    # Access geometry and attribute information for each feature
+                   # geometry = row[0]  # The geometry of the feature (arcpy.Geometry object)
+                    attribute1 = row[1]  # The value of "field1" attribute for the feature
+                    attribute2 = row[2]  # The value of "field2" attribute for the feature
+                    attribute3 = row[3]  # The value of "field3" attribute for the feature
+                    print(attribute3)
+                    data_points.append(("Hurricane_May",attribute3,attribute2,attribute1))
+
+            ## Hurricane MAY ###########################################################################
+
+
+            ## Hurricane  JUNE ###########################################################################
+
+            infeature1 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp"
+            infeature2 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_JUNE.shp"
+            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output_hurricane"
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            folder_path = os.path.join(base_folder, timestamp)
+            os.mkdir(folder_path)
+            ttemp = (os.path.join(folder_path,timestamp))
+            # Process: Find all stream crossings (points)
+            inFeatures = [infeature1, infeature2]
+            intersectOutput = ttemp #os.path.join(folder_path,timestamp) #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output"
+            arcpy.analysis.Intersect(inFeatures, intersectOutput, "", "", "point")
+
+            #print(os.path.join(ttemp + timestamp + ".sh"))
+            print(os.path.join(ttemp + ".shp"))
+            arcpy.management.AddJoin(
+                in_layer_or_view= os.path.join(ttemp + ".shp") , #s.path.join(base_folder, timestamp + ".sh"), #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp",
+                in_field="Id_1",
+                join_table= r"C:\Users\rit13425\Desktop\hackathon\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_JUNE.shp",
+                join_field="Id",
+                index_join_fields="NO_INDEX_JOIN_FIELDS",
+                rebuild_index="NO_REBUILD_INDEX",
+                join_type="KEEP_COMMON"
+            )
+
+            # Path to the shapefile you want to read
+            shapefile_path = os.path.join(ttemp + ".shp")#r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp"
+
+           # data_points = []
+
+            # Open a SearchCursor to read the shapefile
+            fields = ["SHAPE@", "gridcode","Shape","Explain"]  # Replace field1, field2, ... with the field names you want to access
+            with arcpy.da.SearchCursor(shapefile_path, fields) as cursor:
+                for row in cursor:
+                    
+                    # Access geometry and attribute information for each feature
+                   # geometry = row[0]  # The geometry of the feature (arcpy.Geometry object)
+                    attribute1 = row[1]  # The value of "field1" attribute for the feature
+                    attribute2 = row[2]  # The value of "field2" attribute for the feature
+                    attribute3 = row[3]  # The value of "field3" attribute for the feature
+                    print(attribute3)
+                    data_points.append(("Hurricane_JUNE",attribute3,attribute2,attribute1))
+
+            ## Hurricane JUNE ###########################################################################
+
+
+            ## Hurricane  JULY1 ###########################################################################
+
+            infeature1 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp"
+            infeature2 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_JULY1.shp"
+            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output_hurricane"
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            folder_path = os.path.join(base_folder, timestamp)
+            os.mkdir(folder_path)
+            ttemp = (os.path.join(folder_path,timestamp))
+            # Process: Find all stream crossings (points)
+            inFeatures = [infeature1, infeature2]
+            intersectOutput = ttemp #os.path.join(folder_path,timestamp) #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output"
+            arcpy.analysis.Intersect(inFeatures, intersectOutput, "", "", "point")
+
+            #print(os.path.join(ttemp + timestamp + ".sh"))
+            print(os.path.join(ttemp + ".shp"))
+            arcpy.management.AddJoin(
+                in_layer_or_view= os.path.join(ttemp + ".shp") , #s.path.join(base_folder, timestamp + ".sh"), #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp",
+                in_field="Id_1",
+                join_table= r"C:\Users\rit13425\Desktop\hackathon\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_JULY1.shp",
+                join_field="Id",
+                index_join_fields="NO_INDEX_JOIN_FIELDS",
+                rebuild_index="NO_REBUILD_INDEX",
+                join_type="KEEP_COMMON"
+            )
+
+            # Path to the shapefile you want to read
+            shapefile_path = os.path.join(ttemp + ".shp")#r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp"
+
+           # data_points = []
+
+            # Open a SearchCursor to read the shapefile
+            fields = ["SHAPE@", "gridcode","Shape","Explain"]  # Replace field1, field2, ... with the field names you want to access
+            with arcpy.da.SearchCursor(shapefile_path, fields) as cursor:
+                for row in cursor:
+                    
+                    # Access geometry and attribute information for each feature
+                   # geometry = row[0]  # The geometry of the feature (arcpy.Geometry object)
+                    attribute1 = row[1]  # The value of "field1" attribute for the feature
+                    attribute2 = row[2]  # The value of "field2" attribute for the feature
+                    attribute3 = row[3]  # The value of "field3" attribute for the feature
+                    print(attribute3)
+                    data_points.append(("Hurricane_JULY",attribute3,attribute2,attribute1))
+
+            ## Hurricane JULY1 ###########################################################################
+
+
+
+            ## Hurricane  AUG ###########################################################################
+
+            infeature1 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\f2\\NewShapefile.shp"
+            infeature2 = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_AUG.shp"
+            base_folder = "C:\\Users\\rit13425\\Desktop\\hackathon\\georiskpro\\output_hurricane"
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            folder_path = os.path.join(base_folder, timestamp)
+            os.mkdir(folder_path)
+            ttemp = (os.path.join(folder_path,timestamp))
+            # Process: Find all stream crossings (points)
+            inFeatures = [infeature1, infeature2]
+            intersectOutput = ttemp #os.path.join(folder_path,timestamp) #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output"
+            arcpy.analysis.Intersect(inFeatures, intersectOutput, "", "", "point")
+
+            #print(os.path.join(ttemp + timestamp + ".sh"))
+            print(os.path.join(ttemp + ".shp"))
+            arcpy.management.AddJoin(
+                in_layer_or_view= os.path.join(ttemp + ".shp") , #s.path.join(base_folder, timestamp + ".sh"), #r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp",
+                in_field="Id_1",
+                join_table= r"C:\Users\rit13425\Desktop\hackathon\georiskpro\\Hurricane_Data\\Hurricane_Data\\HurricaneRisk_AUG.shp",
+                join_field="Id",
+                index_join_fields="NO_INDEX_JOIN_FIELDS",
+                rebuild_index="NO_REBUILD_INDEX",
+                join_type="KEEP_COMMON"
+            )
+
+            # Path to the shapefile you want to read
+            shapefile_path = os.path.join(ttemp + ".shp")#r"C:\Users\rit13425\Desktop\hackathon\georiskpro\output\output.shp"
+
+           # data_points = []
+
+            # Open a SearchCursor to read the shapefile
+            fields = ["SHAPE@", "gridcode","Shape","Explain"]  # Replace field1, field2, ... with the field names you want to access
+            with arcpy.da.SearchCursor(shapefile_path, fields) as cursor:
+                for row in cursor:
+                    
+                    # Access geometry and attribute information for each feature
+                   # geometry = row[0]  # The geometry of the feature (arcpy.Geometry object)
+                    attribute1 = row[1]  # The value of "field1" attribute for the feature
+                    attribute2 = row[2]  # The value of "field2" attribute for the feature
+                    attribute3 = row[3]  # The value of "field3" attribute for the feature
+                    print(attribute3)
+                    data_points.append(("Hurricane_AUG",attribute3,attribute2,attribute1))
+
+            ## Hurricane JULY1 ###########################################################################
 
             response_data = {"status": "success", "message": "JSON data received successfully", "data": data_points}
             response_status = 200
@@ -149,6 +342,8 @@ class JSONHandler(http.server.BaseHTTPRequestHandler):
         # Set the response headers
         self.send_response(response_status)
         self.send_header('Content-type', 'application/json')
+        # Add the 'Access-Control-Allow-Origin' header to allow requests from all origins
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
         # Send the response data
